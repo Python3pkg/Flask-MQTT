8ed9d2766421fc2337c6acb9e123f53a34f578de
Python3pkg <raliclo@gmail.com>
2017-05-21 06:47:35 -0700

    Convert to Python3

--- doc/_themes/pygments.py
+++ doc/_themes/pygments.py
@@ -1,6 +1,6 @@
 # flasky pygments style based on tango style
-from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, \
+from .pygments.style import Style
+from .pygments.token import Keyword, Name, Comment, String, Error, \
      Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
 
 
